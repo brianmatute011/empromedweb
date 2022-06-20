463029820ad4b06c61cc87e12722f287d295327e
brianmatute011 <brianmatute011@gmail.com>
2022-06-20 02:18:48 -0400

    TProductionController and Reuse code from .twig

---
+++ src/Controller/TProductionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TProductionController extends AbstractController
+{
+    #[Route('/t/production', name: 'app_t_production')]
+    public function index(): Response
+    {
+        return $this->render('t_production/index.html.twig', [
+            'controller_name' => 'TProductionController',
+        ]);
+    }
+}
