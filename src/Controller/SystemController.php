0ef95d14eabda28b653fe90e7ae560d778560756
brianmatute011 <brianmatute011@gmail.com>
2022-06-27 23:59:14 -0400

    Retouch dfetails

--- src/Controller/SystemController.php
+++ src/Controller/SystemController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class SystemController extends AbstractController
 {
     #[Route('/main/system', name: 'app_system')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $getArrayUsers = $doctrine->getRepository(User::class)->findAll();
+
+
         return $this->render('system/index.html.twig', [
             'controller_name' => 'SystemController',
+            'users' => $getArrayUsers
         ]);
     }
 }
